c5e136de6ae8ade63c88479ee4167a972914d68d
Madotzz <rahmatrahmadi920@gmail.com>
2025-06-19 19:14:59 +0800

    harusnya fix

--- app/Models/Delivery.php
+++ app/Models/Delivery.php
@@ -18,4 +18,5 @@ class Delivery extends Model
     {
         return $this->belongsTo(User::class, 'kurir_id');
     }
+    
 }
--- app/Models/Pesanan.php
+++ app/Models/Pesanan.php
@@ -8,18 +8,24 @@ class Pesanan extends Model
 {
     protected $fillable = ['user_id', 'produk_id', 'alamat', 'kontak', 'status'];
 
-    public function transaksi(){
+    public function transaksi()
+    {
         return $this->hasMany(Transaksi::class);
     }
     public function pesanan()
     {
         return $this->belongsToMany(Pesanan::class, 'transaksi', 'produk_id', 'pesanan_id')
-                    ->withPivot('jumlah', 'subtotal_harga')
-                    ->withTimestamps();
+            ->withPivot('jumlah', 'subtotal_harga')
+            ->withTimestamps();
     }
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function delivery()
+    {
+        return $this->hasOne(Delivery::class);
     }
 }
